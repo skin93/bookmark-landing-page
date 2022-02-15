d82fb5a7f9fdd0493ffbdbc9c362aef2ab776aa0
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-02-15 15:55:49 +0100

    Change container class, set font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,css}",
+  ],
+  mode: "jit",
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
   },
   plugins: [],
 };
